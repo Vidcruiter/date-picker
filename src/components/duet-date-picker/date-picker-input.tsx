02cac45db670bf78e1c65a560af55313a40da33c
Thomas Tramley <thomas@caddieservice.co>
2024-06-03 16:31:39 -0300

    Fix week number calculation bug

--- src/components/duet-date-picker/date-picker-input.tsx
+++ src/components/duet-date-picker/date-picker-input.tsx
@@ -25,9 +25,8 @@ type DatePickerInputProps = {
 
 function getWeekNumber(currentDate) {
   const startDate = new Date(currentDate.getFullYear(), 0, 1)
-  const days = Math.floor((currentDate - startDate.getTime()) / (24 * 60 * 60 * 1000))
 
-  return Math.ceil(days / 7)
+  return Math.ceil(((currentDate - startDate.getTime()) / 86400000 + 1) / 7)
     .toString()
     .padStart(2, "0")
 }
